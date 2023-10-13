f11714e69ba0b7c657015cd74ed55e8a9422894b
wellighon <welldarlan.pw@gmail.com>
2023-10-13 16:53:27 -0300

    animação e funçoes

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-const input = document.querySelector(".input-task")
-const btnAddTask = document.querySelector(".button-add-task")
-const listTask = document.querySelector(".list-task")
+const input = document.querySelector(".input-task");
+const btnAddTask = document.querySelector(".button-add-task");
+const listTask = document.querySelector(".list-task");
 
-let minhaLista = []
+let minhaLista = [];
 
-btnAddTask.addEventListener('click', () => {
-  minhaLista.push(input.value)
+btnAddTask.addEventListener("click", adicionarTarefa);
+input.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    adicionarTarefa();
+  }
+});
 
-  input.value = ''
+function adicionarTarefa() {
+  if (input.value === "") {
+    alert("Digite a tarefa a ser adicionada.");
+  } else {
+    const primeiraLetraMaiuscula =
+      input.value.charAt(0).toUpperCase() + input.value.slice(1);
 
-  mostrarTarefas()
-})
+    minhaLista.push({
+      tarefa: primeiraLetraMaiuscula,
+      concluida: false,
+    });
+
+    input.value = "";
+
+    mostrarTarefas();
+  }
+}
 
 function mostrarTarefas() {
 
@@ -19,19 +36,38 @@ function mostrarTarefas() {
   minhaLista.forEach((item, i) => {
 
     liNova = liNova + `
-      <li class="task">
-        <img src="./img/checked.png" alt="checked.png" />
+      <li class="task ${item.concluida && 'done'}">
+        <img src="./img/checked.png" alt="checked.png" onclick="concluirTarefa(${i}) "/>
         <p>
-          ${item}
+          ${item.tarefa}
         </p>
-        <img src="./img/trash.png" alt="trash.png" onclick="deletarItem(${i}) "/>
+        <img src="./img/trash.png" alt="trash.png" onclick="deletarTarefa(${i})"/>
       </li>
     `;
   })
   listTask.innerHTML = liNova
+
+  localStorage.setItem("Lista de tarefas", JSON.stringify(minhaLista))
 }
 
-function deletarItem(i) {
- minhaLista.splice(i, 1)
- mostrarTarefas()
+function concluirTarefa(i) {
+  minhaLista[i].concluida = !minhaLista[i].concluida
+  mostrarTarefas()
 }
+
+function deletarTarefa(i) {
+  minhaLista.splice(i, 1)
+  mostrarTarefas()
+}
+
+function recarregarLS() {
+  const lista = localStorage.getItem('Lista de tarefas')
+
+  if (lista) {
+
+    minhaLista = JSON.parse(lista)
+  }
+  mostrarTarefas()
+}
+
+recarregarLS()
